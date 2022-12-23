a7290fea7e3f58bf6183511ade2b8d024144af55
Adielson Medeiros <medeirosadielson@gmail.com>
2022-12-23 08:49:23 -0300

    balaozera commit

---
+++ estourabalao.js
@@ -0,0 +1,25 @@
+var total = 0
+
+function criarbalao(){
+    var balao = document.createElement("div")
+    balao.setAttribute("class", "balao")
+
+    var x = Math.floor(Math.random() * 600)
+    var y = Math.floor(Math.random() * 400)
+
+    balao.setAttribute("style", "left:"+x+"px;top:"+y+"px;");
+    balao.setAttribute("onclick", "estourar(this)");
+    document.body.appendChild(balao)
+}
+
+function estourar (objeto){
+    document.body.removeChild(objeto)
+    total++
+    var score = document.getElementById("total")
+    score.innerHTML = "Baloes estourados: " + total;
+}
+
+function carregarjogo(){
+    setInterval (criarbalao, 1000);
+
+}
